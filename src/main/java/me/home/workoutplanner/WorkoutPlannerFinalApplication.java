b59f3c8af41876725105b4a0d563e929a9a3a77e
Tarcsa Tamás <tarcsa.tamas90@gmail.com>
2020-02-27 10:29:23 +0100

    Project started

---
+++ src/main/java/me/home/workoutplanner/WorkoutPlannerFinalApplication.java
@@ -0,0 +1,13 @@
+package me.home.workoutplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlannerFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutPlannerFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/home/workoutplanner/WorkoutPlannerFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.home.workoutplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutPlannerFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
